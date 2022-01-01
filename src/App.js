64a1b22f6c908e3f476f236628aaf668b7484e51
YoussefAbdulaziz <azizy5203@gmail.com>
2022-01-01 18:12:23 +0200

    some styling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import StDashboard from './components/StDashboard';
 import StudentCourses from './components/StudentCourses';
+import ProfessorDashboard from './components/ProfessorDashboard';
 import Navbar from './components/Navbar';
+
+
+
+function createtemp(data){
+  return (<ProfessorDashboard
+  emoji={data.emoji}
+  name={data.name}
+  meaning={data.meaning}
+  ></ProfessorDashboard>)
+}
+
+
 
 function App() {
   var [type,setType] = useState("student")
+  //for students pages
   const coursesList = [
     {
       title:'Cyber Security',
@@ -63,6 +77,33 @@ function App() {
       creds:'3hrs',
     },
   ]
+  //for Prof Dashboard
+  const Data = [
+    {
+      id: 1,
+    
+      name: "python",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+
+      name: "oop",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+   
+      name: "Data structure",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    }
+  ];
+
+
+
   return (
     <Router>
       <div className="App">
