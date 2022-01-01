4c632dd059b44df25b593a012cbcb54a1ae14328
YoussefAbdulaziz <azizy5203@gmail.com>
2022-01-01 04:24:59 +0200

    completed student homepage and courses with routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './css/App.css';
-import logo from '../src/assets/logo.svg'
-import noti from '../src/assets/Notifications.svg'
-import person from '../src/assets/person.svg'
+
 import { useState } from 'react';
+import {BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import StDashboard from './components/StDashboard';
 import StudentCourses from './components/StudentCourses';
+import Navbar from './components/Navbar';
 
 function App() {
   var [type,setType] = useState("student")
@@ -33,30 +33,47 @@ function App() {
             advanced topics`,
       creds:'3hrs',
     },
+    {
+      title:'Advanced Python',
+      desc:`This course goes through the 
+            advanced topics`,
+      creds:'3hrs',
+    },
+    {
+      title:'C++ Graphics',
+      desc:`This course goes through the 
+            advanced topics in C++`,
+      creds:'3hrs',
+    },
+    {
+      title:'Advanced ML',
+      desc:`This course goes through the 
+            advanced topics in ML`,
+      creds:'3hrs',
+    },
+    {
+      title:'Advanced Python',
+      desc:`This course goes through the 
+            advanced topics`,
+      creds:'3hrs',
+    },
+    {
+      title:'C++ Graphics',
+      desc:`This course goes through the 
+            advanced topics in C++`,
+      creds:'3hrs',
+    },
   ]
   return (
-    <div className="App">
-      <StudentCourses coursesList={coursesList}/>
-        <header>
-          <img src = {logo} className = "logo" alt=''></img>
-          <span className = "sim">SIM</span>
-          
-          <a href="https://www.google.com"><img src = {noti} className = "notif" alt=''/></a>
-          <a href=""><img src = {person} className = "person" alt=''></img></a>
-          <nav>
-            <ul className = "nav-area">
-              <li><a href = "#">Home</a></li>
-              <li><a href = "#">Attendance</a></li>
-              <li><a href = "#">Grades</a></li>
-              <li><a href = "#">Courses</a></li>
-              <li><a href = "#">Help</a></li>
-            </ul>
-            <span className = "type">{type}</span>
-          </nav>
-      </header>
-      <br />
-      <StDashboard coursesList={coursesList}/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar type={type}/>
+      <Routes>
+          <Route path='/StudentCourses' element={<StudentCourses coursesList={coursesList}/>}/>
+          <Route path='/StDashboard' element={<StDashboard coursesList={coursesList}/>}/>
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import {Link} from 'react-router-dom'
+import logo from '../assets/logo.svg'
+import noti from '../assets/Notifications.svg'
+import person from '../assets/person.svg'
+import '../css/App.css';
+const Navbar = ({type}) => {
+    return (
+        <header>
+            <img src = {logo} className = "logo" alt=''></img>
+            <span className = "sim">SIM</span>
+            
+            <a href="https://www.google.com"><img src = {noti} className = "notif" alt=''/></a>
+            <a href=""><img src = {person} className = "person" alt=''></img></a>
+            <nav>
+                <ul className = "nav-area">
+                <li><Link to = "/stdashboard">Home</Link></li>
+                <li><a href = "#">Attendance</a></li>
+                <li><a href = "#">Grades</a></li>
+                <li><Link to = "/StudentCourses">Courses</Link></li>
+                <li><a href = "#">Help</a></li>
+                </ul>
+                <span className = "type">{type}</span>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
